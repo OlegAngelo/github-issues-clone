08daf3efcdec05c959811a3658bfcf7b49248832
balagtasangelo <ninoangelo.balagtas@sun-asterisk.com>
2021-08-03 11:23:51 +0800

    [GIC-PREP] Modify Initial Setup and delete unnecessary files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import IssuesList from './IssuesList';
+
+const App = () => {
+    return (
+        <div>
+            <IssuesList>
+
+            </IssuesList>
+        </div>
+    );
+};
+
+export default App;
