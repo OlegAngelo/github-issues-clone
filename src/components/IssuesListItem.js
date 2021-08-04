1d62567ce5bc57ca033c80162ed2021e1cff85f3
Angelo <52464397+OlegAngelo@users.noreply.github.com>
2021-08-04 10:23:37 +0800

    [GIC-TASK] Made adjustments for date_info function
    Changed var to const and 'timestamp' variable to 'issue_created_at'

--- src/components/IssuesListItem.js
+++ src/components/IssuesListItem.js
@@ -7,15 +7,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const IssuesListItem = ({ issue }) => {
 
   const date_info = () => {
-    var timestamp = moment(issue.created_at);
-    var current_month = moment().format("M");
-    var github_month = timestamp.format("M");
+    const issue_created_at = moment(issue.created_at);
+    const current_month = moment().format("M");
+    const github_month = issue_created_at.format("M");
 
     if (current_month === github_month) {
-      return timestamp.fromNow();
+      return issue_created_at.fromNow();
     };
         
-    return `on ${timestamp.format("MMM D")}`;
+    return `on ${issue_created_at.format("MMM D")}`;
   };
 
   const issue_labels = issue.labels.map((label) => {
