5b95d867fbbf9153a75c704821d4bfd4aa8c745f
balagtasangelo <ninoangelo.balagtas@sun-asterisk.com>
2021-08-11 10:46:08 +0800

    [GIC-TASK] Added navigation link for user to go back to issues list page

--- src/pages/Issues/Detail/IssueDetails.js
+++ src/pages/Issues/Detail/IssueDetails.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 const IssueDetails = () => {
   const { number } = useParams();
 
   return (
     <div>
+      <div>
+        <Link to='/'>
+          <h5>Github Issues</h5>
+        </Link>
+      </div>
       <h2>Details Page - {number}</h2>
     </div>
   );
