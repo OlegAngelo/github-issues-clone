e2998223784ef84404e0b7ac8138cc8635f012c6
balagtasangelo <ninoangelo.balagtas@sun-asterisk.com>
2021-08-11 18:57:59 +0800

    [GIC-TASK] Display all information inside the Details Page

---
+++ src/pages/Issues/Detail/HeaderDetails.js
@@ -0,0 +1,49 @@
+import React from "react";
+import moment from "moment";
+import { Divider, Header, Segment } from "semantic-ui-react";
+
+const HeaderDetails = ({ details }) => {
+  console.log(details);
+  const { title, number, body, state, user, comments } = details;
+  const { login } = user;
+
+  const dateInfo = () => {
+    const issueCreatedAt = moment(details.created_at);
+    const currentMonth = moment().format("M");
+    const githubMonth = issueCreatedAt.format("M");
+
+    if (currentMonth === githubMonth) {
+      return issueCreatedAt.fromNow();
+    }
+
+    return `on ${issueCreatedAt.format("MMM D")}`;
+  };
+
+  return (
+    <div>
+      <h2>
+        {title} <span className='text-secondary'> #{number} </span>
+      </h2>
+      <div>
+        <span
+          className='rounded-pill mx-3 px-3 py-2 text-capitalize large circle text-white'
+          style={{ backgroundColor: state === "open" ? `green` : `red` }}
+        >
+          {state}
+        </span>
+        <b>{login}</b> opened this issue {dateInfo()} · {comments} comments
+      </div>
+
+      <Divider />
+
+      <Segment className='bg-light' attached='top'>
+        <b>{login}</b> commented {dateInfo()}
+      </Segment>
+      <Segment attached>
+        <p>{body}</p>
+      </Segment>
+    </div>
+  );
+};
+
+export default HeaderDetails;
--- src/pages/Issues/Detail/IssueDetails.js
+++ src/pages/Issues/Detail/IssueDetails.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import HeaderDetails from "./HeaderDetails";
 import { useParams, Link } from "react-router-dom";
+import GithubApi from "../../../apis/GithubApi";
+import { Dimmer, Loader } from "semantic-ui-react";
 
 const IssueDetails = () => {
   const { number } = useParams();
+  const [details, setDetails] = useState(null);
+  const [isFetching, setIsFetching] = useState(true);
+
+  useEffect(() => {
+    GithubApi.fetchIssueDetails(number).then((response) => {
+      setDetails(response);
+      setIsFetching(false);
+    });
+
+  }, []);
 
   return (
     <div>
@@ -11,7 +24,16 @@ const IssueDetails = () => {
           <h5>Github Issues</h5>
         </Link>
       </div>
-      <h2>Details Page - {number}</h2>
+
+      <div>
+        {isFetching ? (
+          <Dimmer active inverted>
+            <Loader inverted>Loading</Loader>
+          </Dimmer>
+        ) : (
+          <HeaderDetails details={details}/>
+        )}
+      </div>
     </div>
   );
 };
